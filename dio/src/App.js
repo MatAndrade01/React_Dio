f10e83c910500c3dcf98bf8a0f942205ac106611
Matheus Andrade <ltbmatheus141012@gmail.com>
2024-07-24 15:14:11 -0300

    Trabalhando com navegação

--- dio/src/App.js
+++ dio/src/App.js
@@ -1,13 +1,22 @@
-import Button from "./components/Button";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Home } from './pages/home'
+import { Login } from './pages/login'
 
 function App() {
   return (
-    <div className="App">
-    <h1>Olá React</h1>
-    <Button title={"Entrar"}/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
   );
-
 }
 
 export default App;
