2e42a5ef66b008a190d95af9720106a75945d952
Matheus Andrade <ltbmatheus141012@gmail.com>
2024-07-25 12:07:56 -0300

    Começando a fazer a tela principal!

--- dio/src/pages/home/index.jsx
+++ dio/src/pages/home/index.jsx
@@ -1,9 +1,8 @@
-import {Link} from 'react-router-dom';
+import { Header } from '../../components/Header/idex';
 
 const Home = () => {
     return (<>
-        <h1>Home</h1>
-        <Link to="/login">Fazer Login</Link>
+        <Header/>
     </>);
 }
 
